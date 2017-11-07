b9168bab5bb2323c2e7387c8ebfcfcde09a2c087
copiner <francised@foxmail.com>
2017-11-07 16:19:27 +0800

    change

--- init/007/container/generic/BasicGeneric.java
+++ init/007/container/generic/BasicGeneric.java
@@ -10,21 +10,12 @@ public class BasicGeneric{
       String s = c.get(i);
       System.out.println(s);
     }
+
     Collection<String> c2 = new HashSet<String>();
     c2.add("aaa");c2.add("bbb");c2.add("ccc");
     for(Iterator<String> it = c2.iterator(); it.hasNext();){
       String s = it.next();
       System.out.println(s);
     }
-  }
-}
-
-class MyName implements Comparable<MyName>{
-  int age;
-
-  public int compareTo(MyName mn){
-    if(this.age > mn.age) return 1;
-    else if(this.age < mn.age) return -1;
-    else return 0;
   }
 }
